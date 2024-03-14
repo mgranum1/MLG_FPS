7826a4466b7c43fe125efc407f6da8e448388ea0
mgranum1 <266515@stud.inn.no>
2024-03-14 09:52:46 +0100

    added enhanced input movement system

--- Source/MLG_FPS/Character/MLGFPSCharacter.cpp
+++ Source/MLG_FPS/Character/MLGFPSCharacter.cpp
@@ -1,52 +1,102 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MLG_FPS/Character/MLGFPSCharacter.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "InputAction.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 
-// Sets default values     //Constructor
 AMLGFPSCharacter::AMLGFPSCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	SKFPV = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPSMesh"));
+	///*SpringArm Component*/
+	//SpingArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	//SpingArm->SetupAttachment(GetRootComponent());
 
+	/*Camera Component*/
+	FPVCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	FPVCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FPVCameraComponent->bUsePawnControlRotation = true;
+
+	/*Skeletal Mesh Component*/
+	FPVMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharMesh"));
+	FPVMesh->SetupAttachment(FPVCameraComponent);
 }
 
-void AMLGFPSCharacter::Move(FInputActionValue)
+void AMLGFPSCharacter::Move(const FInputActionValue& Value)
 {
-	//TO-DO MAKE THE CHARACTER MOVE
+	GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Emerald, TEXT("Triggering the move function"));
+
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
 }
 
-// Called when the game starts or when spawned
+void AMLGFPSCharacter::LookAround(const FInputActionValue& Value)
+{
+	FVector2D LookAroundVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAroundVector.X);
+		AddControllerPitchInput(LookAroundVector.Y);
+	}
+}
+
+void AMLGFPSCharacter::SetHasWeapon(bool bHasNewWeapon)
+{
+	bHasWeapon = bHasNewWeapon;
+}
+
+bool AMLGFPSCharacter::GetHasWeapon()
+{
+	return bHasWeapon;
+}
+
+USkeletalMeshComponent* AMLGFPSCharacter::GetMeshFPV() const
+{
+	return FPVMesh;
+}
+
+UCameraComponent* AMLGFPSCharacter::GetFPVCameraComponent() const
+{
+	return FPVCameraComponent;
+}
+
 void AMLGFPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Adding the Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(IMC, 0);
+		}
+	}
 }
 
-// Called every frame
 void AMLGFPSCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
 void AMLGFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-
 	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
 
 	if (EnhancedInputComponent)
 	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMLGFPSCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMLGFPSCharacter::LookAround);
 
-		//EnhancedInputComponent->BindAction(MoveAction)
-
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 	}
-
 }
-
--- Source/MLG_FPS/Character/MLGFPSCharacter.h
+++ Source/MLG_FPS/Character/MLGFPSCharacter.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnhancedInputSubsystemInterface.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
 #include "MLGFPSCharacter.generated.h"
 
@@ -12,30 +14,66 @@ class uInputAction;
 
 
 UCLASS()
-class MLG_FPS_API AMLGFPSCharacter : public ACharacter //A prefix for ACTOR
-{													//U prefor for OBJECT
+class MLG_FPS_API AMLGFPSCharacter : public ACharacter
+{
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AMLGFPSCharacter();
-// Called every frame
+
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+
+	/*
+	 * Components
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	USkeletalMeshComponent* SKFPV;
+	UCameraComponent* FPVCameraComponent;
 
-	//Input Actions
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	//USpringArmComponent* SpingArm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USkeletalMeshComponent* FPVMesh;
+
+	/*
+	 * Input Mapping Context and Actions
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
-	UInputAction* MoveAction; 
+	UInputMappingContext* IMC;
 
-	void Move(FInputActionValue)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
 
-protected: //all children of this derived class will be able to access it
-	// Called when the game starts or when spawned
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+
+	void Move(const FInputActionValue& Value);
+
+	void LookAround(const FInputActionValue& Value);
+
+	/*
+	 * Weapon
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+	bool bHasWeapon = false;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void SetHasWeapon(bool bHasNewWeapon);
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	bool GetHasWeapon();
+
+	USkeletalMeshComponent* GetMeshFPV() const;
+
+	UCameraComponent* GetFPVCameraComponent() const;
+
+
+protected:
 	virtual void BeginPlay() override;
-
 };
+
