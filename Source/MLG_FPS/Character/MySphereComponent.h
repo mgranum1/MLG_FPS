f4a0dde92d45eef03f574f85d27f537b1d3de630
mgranum1 <266515@stud.inn.no>
2024-03-14 11:01:31 +0100

    commit

---
+++ Source/MLG_FPS/Character/MySphereComponent.h
@@ -0,0 +1,21 @@
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Components/SphereComponent.h"
+//#include "MySphereComponent.generated.h"
+//
+///**
+// * 
+// */
+//UCLASS()
+//class MLG_FPS_API UMySphereComponent : public USphereComponent
+//{
+//	GENERATED_BODY()
+//
+//
+//
+//
+//
+//};
